2dec65cd387ca7dd2e303203ca6a2c7f16084e75
Shane Mulligan <mullikine@gmail.com>
2014-11-13 13:55:58 +1300

    added help file from version 0.3.2

--- dist/zipsrc.bat
+++ dist/zipsrc.bat
@@ -1,1 +1,2 @@
+set %1=1.1.1
 "%ProgramFiles%\7-zip\7z" -Tzip a source-%1.zip aokts.ini *.rc *.vcproj aokts.sln COPYING.txt data_aok.xml data_swgb.xml readme_aok.txt readme_swgb.txt *.c *.h scx_format.txt src_notes.txt zipsrc.bat model res util view 
